14b616f7eb9231487ca94a31cf99398547e8f1bd
Cory Zachman <cory.zachman@bitmovin.com>
2018-11-08 13:17:43 -0700

    removing BitmovinYospaceConfiguration

--- BitmovinYoSpaceModule/Classes/YospaceSourceConfiguration.swift
+++ BitmovinYoSpaceModule/Classes/YospaceSourceConfiguration.swift
@@ -9,7 +9,11 @@ import UIKit
 import BitmovinPlayer
 
 public class YospaceSourceConfiguration {
-    let yoSpaceAssetType: YoSpaceAssetType
+    public let yoSpaceAssetType: YoSpaceAssetType
+    public var timeout:TimeInterval = 5000
+    public var debug:Bool = false;
+    public var userAgent:String = "BitmovinYospacePlayer"
+    
     public init(yoSpaceAssetType:YoSpaceAssetType){
         self.yoSpaceAssetType = yoSpaceAssetType
     }
