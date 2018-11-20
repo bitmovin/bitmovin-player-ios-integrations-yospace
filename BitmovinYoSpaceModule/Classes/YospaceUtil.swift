5d97fb9918ee19674329508528c74ce894e3ffbf
Cory Zachman <cory.zachman@bitmovin.com>
2018-11-19 23:34:17 -0700

    properly destroy player and resolve all swiftlint issues

--- BitmovinYoSpaceModule/Classes/YospaceUtil.swift
+++ BitmovinYoSpaceModule/Classes/YospaceUtil.swift
@@ -5,43 +5,39 @@ import BitmovinPlayer
 extension YSTimedMetadata {
     public static func createFromMetadata (event: MetadataEvent) -> YSTimedMetadata {
         let meta = YSTimedMetadata()
-        for entry: MetadataEntry in event.metadata.entries {
-            if (entry.metadataType == BMPMetadataType.ID3) {
-                let metadata = entry as! AVMetadataItem
+        for entry: MetadataEntry in event.metadata.entries where entry.metadataType == BMPMetadataType.ID3 {
+            guard let metadata = entry as? AVMetadataItem else {
+                continue
+            }
 
-                guard let key = metadata.key, let data = metadata.dataValue else {
-                    continue
+            guard let key = metadata.key, let data = metadata.dataValue else {
+                continue
+            }
+
+            switch key.description {
+            case "YPRG":
+                NSLog("Programme metadata - ignoring")
+            case "YTYP":
+                if let type  = String(data: data, encoding: String.Encoding.utf8) {
+                    meta.type = String(type[type.index(type.startIndex, offsetBy: 1)...])
+                }
+            case "YSEQ":
+                if let seq = String(data: data, encoding: String.Encoding.utf8) {
+                    meta.setSequenceFrom(String(seq[seq.index(seq.startIndex, offsetBy: 1)...]))
                 }
 
-                switch key.description {
-                case "YPRG":
-                    NSLog("Programme metadata - ignoring")
-
-                case "YTYP":
-                    if let type  = String(data: data, encoding: String.Encoding.utf8) {
-                        meta.type = String(type[type.index(type.startIndex, offsetBy: 1)...])
+            case "YMID":
+                if let mediaID = String(data: data, encoding: String.Encoding.utf8) {
+                    meta.mediaId = String(mediaID[mediaID.index(mediaID.startIndex, offsetBy: 1)...])
+                }
+            case "YDUR":
+                if let offset = String(data: data, encoding: String.Encoding.utf8) {
+                    if let offset = Double(String(offset[offset.index(offset.startIndex, offsetBy: 1)...])) {
+                        meta.offset = offset
                     }
-
-                case "YSEQ":
-                    if let seq = String(data: data, encoding: String.Encoding.utf8) {
-                        meta.setSequenceFrom(String(seq[seq.index(seq.startIndex, offsetBy: 1)...]))
-                    }
-
-                case "YMID":
-                    if let mediaID = String(data: data, encoding: String.Encoding.utf8) {
-                        meta.mediaId = String(mediaID[mediaID.index(mediaID.startIndex, offsetBy: 1)...])
-                    }
-
-                case "YDUR":
-                    if let offset = String(data: data, encoding: String.Encoding.utf8) {
-                        if let offset = Double(String(offset[offset.index(offset.startIndex, offsetBy: 1)...])) {
-                            meta.offset = offset
-                        }
-                    }
-
-                default:
+                }
+            default:
                     break
-                }
             }
         }
         return meta
@@ -49,7 +45,7 @@ extension YSTimedMetadata {
 }
 
 class YospaceUtil {
-
+    // swiftlint:disable cyclomatic_complexity
     static func trackingEventString(event: YSETrackingEvent) -> String {
         switch event {
         case .creativeViewEvent:
@@ -108,4 +104,6 @@ class YospaceUtil {
             return "nonLinearClickTrackingEvent"
         }
     }
+    // swiftlint:enable cyclomatic_complexity
+
 }
