3eaa5c8e2dee0c068186cfb4bfcd0d52443e2768
Cory Zachman <cory.zachman@bitmovin.com>
2019-08-29 19:21:27 -0600

    properly populate liveAdBreak and liveAd objects

--- BitmovinYoSpaceModule/Classes/AdTimeline.swift
+++ BitmovinYoSpaceModule/Classes/AdTimeline.swift
@@ -25,14 +25,29 @@ public class AdBreak: CustomDebugStringConvertible {
         self.relativeStart = relativeStart
     }
 
+    init(ysAdBreak: YSAdBreak) {
+        self.identifier = ysAdBreak.adBreakIdentifier()
+        self.absoluteStart = ysAdBreak.adBreakStart()
+        self.absoluteEnd = ysAdBreak.adBreakEnd()
+        self.duration = ysAdBreak.adBreakDuration()
+        self.relativeStart = ysAdBreak.adBreakStart()
+        
+        for advertisement in ysAdBreak.adverts() {
+            if let advert: YSAdvert = advertisement as? YSAdvert {
+                let newAd: Ad = Ad(ysAd: advert, relativeStart: self.relativeStart)
+                self.appendAd(ad: newAd)
+            }
+        }
+    }
+
     //swiftlint:disable identifier_name
     func appendAd(ad: Ad) {
         self.ads.append(ad)
     }
     //swiftlint:enable identifier_name
-    
+
     public var debugDescription: String {
-        return "id=\(self.identifier) absoluteStart=\(self.absoluteStart) absoluteEnd=\(self.absoluteEnd) ads=\(ads.map{$0.identifier})"
+        return "id=\(self.identifier) absoluteStart=\(self.absoluteStart) absoluteEnd=\(self.absoluteEnd) ads=\(ads.map {$0.identifier})"
     }
 
 }
@@ -55,6 +70,15 @@ public class Ad: CustomDebugStringConvertible {
         self.hasInteractiveUnit = hasInteractiveUnit
     }
     
+    init (ysAd: YSAdvert, relativeStart: TimeInterval) {
+        self.identifier = ysAd.advertIdentifier()
+        self.absoluteStart = ysAd.advertStart()
+        self.absoluteEnd = ysAd.advertEnd()
+        self.duration = ysAd.advertDuration()
+        self.relativeStart = relativeStart
+        self.hasInteractiveUnit = ysAd.hasLinearInteractiveUnit()
+    }
+
     public var debugDescription: String {
         return "id=\(self.identifier) absoluteStart=\(self.absoluteStart) absoluteEnd=\(self.absoluteEnd)"
     }
