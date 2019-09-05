d7b334707b6059fcddbe8d1149c3df610884d053
Cory Zachman <cory.zachman@bitmovin.com>
2019-09-04 19:11:32 -0600

    store the correct startOffset to send to yospace

--- BitmovinYoSpaceModule/Classes/TimeRange.swift
+++ BitmovinYoSpaceModule/Classes/TimeRange.swift
@@ -17,7 +17,7 @@ public class TimeRange {
      * end of the time range
      */
     public var end: TimeInterval
-    
+
     public init(start: TimeInterval, end: TimeInterval) {
         self.start = start
         self.end = end
