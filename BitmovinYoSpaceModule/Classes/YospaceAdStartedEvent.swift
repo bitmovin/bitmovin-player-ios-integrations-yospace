df5e3000b3865de2cd77f6243f52fa45d4413bba
Cory Zachman <cory.zachman@bitmovin.com>
2019-09-20 11:27:23 -0600

    notify users when an ad is truex

---
+++ BitmovinYoSpaceModule/Classes/YospaceAdStartedEvent.swift
@@ -0,0 +1,13 @@
+//
+//  TrueXAdStartedEvent.swift
+//  Pods
+//
+//  Created by Cory Zachman on 9/20/19.
+//
+
+import Foundation
+import BitmovinPlayer
+
+public class YospaceAdStartedEvent: AdStartedEvent {
+    public var truexAd: Bool = false
+}
