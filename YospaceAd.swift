ec70976d83abadb9b8c2bca53f6c607ad86b06f2
aneurinc <nsc9012@gmail.com>
2020-02-14 16:48:06 -0700

    Made YospaceAd conform to Ad protocol

--- YospaceAd.swift
+++ YospaceAd.swift
@@ -6,30 +6,54 @@
 //
 
 import Foundation
+import BitmovinPlayer
 
 //swiftlint:disable type_name
-public class YospaceAd: CustomDebugStringConvertible {
+public class YospaceAd: NSObject, Ad {
     public private(set) var relativeStart: TimeInterval = 0.0
-    public private(set) var identifier: String = "unknown"
+    public var identifier: String? = "unknown"
     public private(set) var duration: TimeInterval = 0.0
     public private(set) var hasInteractiveUnit = false
     public private(set) var absoluteStart: TimeInterval = 0.0
     public private(set) var absoluteEnd: TimeInterval = 0.0
-    public private(set) var clickThroughUrl: URL?
+    public var clickThroughUrl: URL?
+    public var isLinear: Bool
+    public var width: Int
+    public var height: Int
+    public var mediaFileUrl: URL?
+    public var data: AdData?
 
-    init(identifier: String, absoluteStart: TimeInterval, absoluteEnd: TimeInterval, duration: TimeInterval, relativeStart: TimeInterval, hasInteractiveUnit: Bool, clickThroughUrl: URL?) {
+    init(identifier: String, absoluteStart: TimeInterval, absoluteEnd: TimeInterval, duration: TimeInterval, relativeStart: TimeInterval, hasInteractiveUnit: Bool, isLinear: Bool, clickThroughUrl: URL?, data: AdData?) {
         self.identifier = identifier
         self.absoluteStart = absoluteStart
         self.absoluteEnd = absoluteEnd
         self.duration = duration
         self.relativeStart = relativeStart
+        self.isLinear = isLinear
         self.hasInteractiveUnit = hasInteractiveUnit
         self.clickThroughUrl = clickThroughUrl
+        self.data = data
+        
+        // Not supported
+        self.width = -1
+        self.height = -1
+        self.mediaFileUrl = nil
     }
-
-    public var debugDescription: String {
-        return "id=\(self.identifier) absoluteStart=\(self.absoluteStart) absoluteEnd=\(self.absoluteEnd)"
+    
+    public func toJsonString() throws -> String {
+        return ""
     }
-
+    
+    public func toJsonData() -> [AnyHashable : Any] {
+        return [:]
+    }
+    
+    public static func fromJsonData(_ jsonData: [AnyHashable : Any]) throws -> Any {
+        return jsonData
+    }
+    
+    override public var debugDescription: String {
+        return "id=\(self.identifier ?? "") absoluteStart=\(self.absoluteStart) absoluteEnd=\(self.absoluteEnd)"
+    }
 }
 //swiftlint:enable type_name
