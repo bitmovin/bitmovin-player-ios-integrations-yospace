53b49362b467fad957b14a0fe44cc5ed0b2a362a
aneurinc <nsc9012@gmail.com>
2020-03-03 14:14:52 -0700

    Add truex tracking

---
+++ BitmovinYoSpaceModule/Classes/BitmovinTruexRendererDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  BitmovinTruexRendererDelegate.swift
+//  Pods
+//
+//  Created by aneurinc on 3/3/20.
+//
+
+import Foundation
+
+protocol BitmovinTruexRendererDelegate: AnyObject {
+    func truexAdComplete()
+    func truexAdFree()
+}
