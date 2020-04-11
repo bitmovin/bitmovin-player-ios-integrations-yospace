90ac39ae6a8bcaaa5e8c902684670433c97792ca
aneurinc <nsc9012@gmail.com>
2020-04-11 14:55:53 -0600

    Update sample app

---
+++ Example/BitmovinYoSpaceModule/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  BitmovinYospaceModule
+//
+//  Created by aneurinc on 4/11/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+// CNN
+let cnnUrl = "https://live-manifests-aka-qa.warnermediacdn.com/csmp/cmaf/live/2000073/cnn-clear-novpaid/master.m3u8?yo.av=2&yo.dr=true&yo.pst=true"
+
+// TBSE
+let tbseUrl = "https://live-manifests-aka-qa.warnermediacdn.com/csmp/cmaf/live/2011915/tbseast-cbcs-stage/master_fp.m3u8?yo.pdt=true&_fw_ae=53da17a30bd0d3c946a41c86cb5873f1&_fw_ar=1&afid=180483280&conf_csid=tbs.com_desktop_live_east&nw=42448&prof=48804:tbs_ios_live&yo.vp=false&yo.ad=true&yo.av=2&yo.dr=true&yo.pst=true"
+let fairplayLicenseUrl = "https://fairplay-stage.license.istreamplanet.com/api/license/de4c1d30-ac22-4669-8824-19ba9a1dc128"
+let fairplayCertUrl = "https://fairplay-stage.license.istreamplanet.com/api/AppCert/de4c1d30-ac22-4669-8824-19ba9a1dc128"
+
+// Bones
+let bonesVodUrl = "https://vod-manifests-aka-qa.warnermediacdn.com/csm/tcm/clear/3c3c3c3c3c3c3c3c3c3c3c3c3c3c3c3c/master_cl.m3u8?afid=222591187&caid=2100555&conf_csid=tbs.com_videopage&context=182883174&nw=42448&prof=48804%3Atbs_web_vod&vdur=1800&yo.vp=true&yo.av=2"
+let bonesTruexUrl = "https://vod-manifests-aka-qa.warnermediacdn.com/csm/tcm/clear/3c3c3c3c3c3c3c3c3c3c3c3c3c3c3c3c/master_cl.m3u8?afid=222591187&caid=2100555&conf_csid=tbs.com_mobile_iphone&context=182883174&nw=42448&prof=48804:turner_ssai_truex&vdur=1800&yo.vp=true&yo.ad=true&yo.av=2"
+
+// Non YS
+let nonYospaceUrl = "https://hls.pro34.lv3.cdn.hbo.com/av/videos/series/watchmen/videos/trailer/trailer-47867523_PRO34/base_index.m3u8"
