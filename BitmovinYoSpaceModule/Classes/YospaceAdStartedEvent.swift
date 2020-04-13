5b215950f182cde41fdafe6a47e2d82ac489914e
aneurinc <nsc9012@gmail.com>
2020-04-13 14:19:54 -0600

    Add truexAd to YospaceAdStartedEvent init

--- BitmovinYoSpaceModule/Classes/YospaceAdStartedEvent.swift
+++ BitmovinYoSpaceModule/Classes/YospaceAdStartedEvent.swift
@@ -9,5 +9,10 @@ import Foundation
 import BitmovinPlayer
 
 public class YospaceAdStartedEvent: AdStartedEvent {
-    public var truexAd: Bool = false
+    public var truexAd: Bool
+    
+    init(clickThroughUrl: URL?, clientType: BMPAdSourceType, indexInQueue: UInt, duration: TimeInterval, timeOffset: TimeInterval, skipOffset: TimeInterval, position: String?, ad: Ad?, truexAd: Bool = false) {
+        self.truexAd = truexAd
+        super.init(clickThroughUrl: clickThroughUrl, clientType: clientType, indexInQueue: indexInQueue, duration: duration, timeOffset: timeOffset, skipOffset: skipOffset, position: position, ad: ad)
+    }
 }
