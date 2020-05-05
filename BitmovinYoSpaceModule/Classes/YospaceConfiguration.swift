7468ea79098f86c9bc86145d58e8921a1f430c92
aneurinc <nsc9012@gmail.com>
2020-05-05 09:51:24 -0600

    Fix incorrect logging

--- BitmovinYoSpaceModule/Classes/YospaceConfiguration.swift
+++ BitmovinYoSpaceModule/Classes/YospaceConfiguration.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public class YospaceConfiguration {
     // MARK: - Yospace Configuration attributes
-    let debug: Bool?
+    let isDebugEnabled: Bool?
     let userAgent: String?
     let timeout: TimeInterval?
     let pollingInterval: Int?
@@ -20,13 +20,13 @@ public class YospaceConfiguration {
      Initialize a new YospaceConfiguration object to change default behavior of the Yospace Ad Management SDK
      
      - Parameters:
-     - debug: flag that enables debug logging of the Yospace Ad Management SDK
+     - isDebugEnabled: flag that enables debug logging of the Yospace Ad Management SDK
      - userAgent: Custom user agent that is sent with Yospace HTTP requests
      - timeout: HTTP timeout value in millisenconds to be used for Yospace HTTP requests
      
      */
-    public init (debug: Bool? = nil, userAgent: String? = nil, timeout: TimeInterval? = nil, pollingInterval: Int? = nil) {
-        self.debug = debug
+    public init(isDebugEnabled: Bool? = nil, userAgent: String? = nil, timeout: TimeInterval? = nil, pollingInterval: Int? = nil) {
+        self.isDebugEnabled = isDebugEnabled
         self.userAgent = userAgent
         self.timeout = timeout
         self.pollingInterval = pollingInterval
