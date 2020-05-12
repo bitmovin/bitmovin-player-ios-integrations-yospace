f08bc1a7d31b59e274db68690ecb80bc400ac0dc
aneurinc <nsc9012@gmail.com>
2020-05-12 14:57:25 -0600

    Replace slot type with ad break position

--- BitmovinYoSpaceModule/Classes/BitmovinYospacePlayer.swift
+++ BitmovinYoSpaceModule/Classes/BitmovinYospacePlayer.swift
@@ -387,8 +387,8 @@ extension BitmovinYospacePlayer: YSAnalyticObserver {
             BitLog.d("Pausing player")
             super.pause()
             
-            let slotType: TruexSlotType = activeAdBreak?.relativeStart == 0 ? .preroll : .midroll
-            renderer.renderTruexAd(ad: advert, slotType: slotType)
+            let adBreakPosition: AdBreakPosition = activeAdBreak?.relativeStart == 0 ? .preroll : .midroll
+            renderer.renderTruexAd(ad: advert, adBreakPosition: adBreakPosition)
         }
         #endif
 
@@ -499,7 +499,7 @@ extension BitmovinYospacePlayer: YSAnalyticObserver {
     private func handleAdBreakEvent(_ adBreak: YSAdBreak) {
         activeAdBreak = createActiveAdBreak(adBreak: adBreak)
 
-        BitLog.d("Emitting AdBreakStartedEvent")
+        BitLog.d("Emitting AdBreakStartedEvent: position=\(adBreak.adBreakPosition().rawValue)")
         let adBreakStartEvent = AdBreakStartedEvent(adBreak: activeAdBreak!)
         for listener: PlayerListener in listeners {
             listener.onAdBreakStarted?(adBreakStartEvent)
