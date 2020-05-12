426b62c1a041dbee363947cc717cf14c6f598cd6
aneurinc <nsc9012@gmail.com>
2020-05-12 10:21:11 -0600

    Expose ad break position property

---
+++ BitmovinYoSpaceModule/Classes/AdBreakPosition.swift
@@ -0,0 +1,12 @@
+//
+//  AdBreakPosition.swift
+//  Pods
+//
+//  Created by aneurinc on 5/12/20.
+//
+
+import Foundation
+
+public enum AdBreakPosition {
+    case preroll, midroll, postroll, unknown
+}
