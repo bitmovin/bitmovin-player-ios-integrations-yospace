63c9ee8be139765b6fb8c3f7e9c8cd25885ed96d
aneurinc <aneurin@bitmovin.com>
2021-01-13 11:27:52 -0700

    Add missing props to ad

--- BitmovinYoSpaceModule/Classes/YospaceAd.swift
+++ BitmovinYoSpaceModule/Classes/YospaceAd.swift
@@ -11,43 +11,53 @@ import Yospace
 
 public class YospaceAd: NSObject, Ad {
     public var identifier: String?
-    public private(set) var absoluteStart: TimeInterval
-    public private(set) var absoluteEnd: TimeInterval
-    public private(set) var duration: TimeInterval
-    public private(set) var relativeStart: TimeInterval
-    public private(set) var hasInteractiveUnit: Bool
-    public private(set) var isLinear: Bool
+    public let creativeId: String?
+    public let sequence: String?
+    public let absoluteStart: TimeInterval
+    public let relativeStart: TimeInterval
+    public let duration: TimeInterval
+    public let absoluteEnd: TimeInterval
+    public let system: String?
+    public let title: String?
+    public let advertiser: String?
+    public let hasInteractiveUnit: Bool
+    public let extensions: [YSXmlNode]
+    public let isFiller: Bool
+    public let isLinear: Bool
     public var clickThroughUrl: URL?
-    public private(set) var extensions: [YSXmlNode]
-
-    // Not supported
+    public var data: AdData?
     public var width: Int = -1
     public var height: Int = -1
     public var mediaFileUrl: URL?
-    public var data: AdData?
 
-    required init(identifier: String?, absoluteStart: TimeInterval, absoluteEnd: TimeInterval, duration: TimeInterval, relativeStart: TimeInterval, hasInteractiveUnit: Bool = false, isLinear: Bool = false, clickThroughUrl: URL? = nil, extensions: [YSXmlNode] = [YSXmlNode]()) {
+    required init(identifier: String?, creativeId: String?, sequence: String?, absoluteStart: TimeInterval, relativeStart: TimeInterval, duration: TimeInterval, absoluteEnd: TimeInterval, system: String?, title: String?, advertiser: String?, hasInteractiveUnit: Bool, extensions: [YSXmlNode], isFiller: Bool, isLinear: Bool, clickThroughUrl: URL?, mediaFileUrl: URL?) {
         self.identifier = identifier
+        self.creativeId = creativeId
+        self.sequence = sequence
         self.absoluteStart = absoluteStart
-        self.absoluteEnd = absoluteEnd
-        self.duration = duration
         self.relativeStart = relativeStart
-        self.isLinear = isLinear
+        self.duration = duration
+        self.absoluteEnd = absoluteEnd
+        self.system = system
+        self.title = title
+        self.advertiser = advertiser
         self.hasInteractiveUnit = hasInteractiveUnit
-        self.clickThroughUrl = clickThroughUrl
         self.extensions = extensions
+        self.isFiller = isFiller
+        self.isLinear = isLinear
+        self.clickThroughUrl = clickThroughUrl
+        self.mediaFileUrl = mediaFileUrl
     }
 
     override public var debugDescription: String {
         // swiftlint:disable line_length
-        return "id=\(identifier ?? "unknown"), relativeStart=\(relativeStart), absoluteStart=\(absoluteStart), duration=\(duration), absoluteEnd=\(absoluteEnd)"
+        return "id=\(identifier ?? "unknown"), creativeId=\(creativeId ?? "") sequence=\(sequence ?? ""), absoluteStart=\(absoluteStart), relativeStart=\(relativeStart), duration=\(duration), absoluteEnd=\(absoluteEnd), system=\(system ?? ""), title=\(title ?? ""), advertiser=\(advertiser ?? ""), hasInteractiveUnit=\(hasInteractiveUnit), isFiller=\(isFiller), isLinear=\(isLinear), clickThroughUrl=\(clickThroughUrl)"
         // swiftlint:enable line_length
     }
 }
 
 // Implementation of protocol is required, but we do not need to support JSON mapping, so default values are used
 extension YospaceAd: BMPJsonable {
-
     public func toJsonString() throws -> String {
         return ""
     }
@@ -57,12 +67,6 @@ extension YospaceAd: BMPJsonable {
     }
 
     public static func fromJsonData(_ jsonData: [AnyHashable: Any]) throws -> Self {
-        return Self.init(
-            identifier: "",
-            absoluteStart: 0,
-            absoluteEnd: 0,
-            duration: 0,
-            relativeStart: 0
-        )
+        return Self.init(identifier: nil, creativeId: nil, sequence: nil, absoluteStart: 0, relativeStart: 0, duration: 0, absoluteEnd: 0, system: nil, title: nil, advertiser: nil, hasInteractiveUnit: false, extensions: [], isFiller: false, isLinear: false, clickThroughUrl: nil, mediaFileUrl: nil)
     }
 }
