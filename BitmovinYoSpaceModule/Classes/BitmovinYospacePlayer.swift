63ba349d3ca3c1394c3be52e2ca1d54093184231
aneurinc <aneurin@bitmovin.com>
2021-01-13 14:47:47 -0700

    Reorder map functions

--- BitmovinYoSpaceModule/Classes/BitmovinYospacePlayer.swift
+++ BitmovinYoSpaceModule/Classes/BitmovinYospacePlayer.swift
@@ -1031,6 +1031,32 @@ extension BitmovinYospacePlayer: PlayerListener {
     }
 }
 
+// MARK: AdBreak Transformation
+
+extension YSAdBreak {
+    func toYospaceAdBreak(absoluteStart: Double, relativeStart: Double) -> YospaceAdBreak {
+        let yospaceAdBreak = YospaceAdBreak(
+            identifier: adBreakIdentifier(),
+            absoluteStart: absoluteStart,
+            absoluteEnd: absoluteStart + adBreakDuration(),
+            duration: adBreakDuration(),
+            relativeStart: relativeStart,
+            scheduleTime: 0,
+            replaceContentDuration: 0,
+            position: adBreakPosition().toAdBreakPosition()
+        )
+
+        // Add adverts to ad break
+        var adAbsoluteStart = absoluteStart
+        for case let advert as YSAdvert in adverts() {
+            yospaceAdBreak.register(advert.toYospaceAd(absoluteStart: adAbsoluteStart, relativeStart: relativeStart))
+            adAbsoluteStart += advert.advertDuration()
+        }
+
+        return yospaceAdBreak
+    }
+}
+
 // MARK: - Ad Transformation
 
 extension YSAdvert {
@@ -1056,31 +1082,5 @@ extension YSAdvert {
             clickThroughUrl: linearCreativeElement().linearClickthroughURL(),
             mediaFileUrl: linearCreativeElement().interactiveUnit()?.unitSource()
         )
-    }
-}
-
-// MARK: AdBreak Transformation
-
-extension YSAdBreak {
-    func toYospaceAdBreak(absoluteStart: Double, relativeStart: Double) -> YospaceAdBreak {
-        let yospaceAdBreak = YospaceAdBreak(
-            identifier: adBreakIdentifier(),
-            absoluteStart: absoluteStart,
-            absoluteEnd: absoluteStart + adBreakDuration(),
-            duration: adBreakDuration(),
-            relativeStart: relativeStart,
-            scheduleTime: 0,
-            replaceContentDuration: 0,
-            position: adBreakPosition().toAdBreakPosition()
-        )
-
-        // Add adverts to ad break
-        var adAbsoluteStart = absoluteStart
-        for case let advert as YSAdvert in adverts() {
-            yospaceAdBreak.register(advert.toYospaceAd(absoluteStart: adAbsoluteStart, relativeStart: relativeStart))
-            adAbsoluteStart += advert.advertDuration()
-        }
-
-        return yospaceAdBreak
     }
 }
