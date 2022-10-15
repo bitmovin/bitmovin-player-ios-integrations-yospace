bfa1aa4f39cf909adad81eb45f2b42dcf6071c7c
Kun Wu <kun.wu@eyevinn.se>
2022-10-15 21:12:45 +0200

    clean up

--- BitmovinYoSpaceModule/Classes/YospaceUtil.swift
+++ BitmovinYoSpaceModule/Classes/YospaceUtil.swift
@@ -48,69 +48,6 @@ extension YOTimedMetadata {
 }
 
 class YospaceUtil {
-    // swiftlint:disable cyclomatic_complexity
-//    static func trackingEventString(event: YSETrackingEvent) -> String {
-//        switch event {
-//        case .creativeViewEvent:
-//            return "creativeViewEvent"
-//        case .acceptInvitationEvent:
-//            return "acceptInvitationEvent"
-//        case .acceptInvitationLinearEvent:
-//            return "acceptInvitationLinearEvent"
-//        case .clickTrackingEvent:
-//            return "clickTrackingEvent"
-//        case .closeEvent:
-//            return "closeEvent"
-//        case .closeLinearEvent:
-//            return "closeLinearEvent"
-//        case .collapseEvent:
-//            return "collapseEvent"
-//        case .completeEvent:
-//            return "completeEvent"
-//        case .exitfullscreenEvent:
-//            return "exitfullscreenEvent"
-//        case .expandEvent:
-//            return "expandEvent"
-//        case .firstQuartileEvent:
-//            return "firstQuartileEvent"
-//        case .fullscreenEvent:
-//            return "fullscreenEvent"
-//        case .iconClickTrackingEvent:
-//            return "iconClickTrackingEvent"
-//        case .iconViewTrackingEvent:
-//            return "iconViewTrackingEvent"
-//        case .impressionEvent:
-//            return "impressionEvent"
-//        case .midpointEvent:
-//            return "midpointEvent"
-//        case .muteEvent:
-//            return "muteEvent"
-//        case .generalTrackingEvent:
-//            return "generalTrackingEvent"
-//        case .startEvent:
-//            return "startEvent"
-//        case .thirdQuartileEvent:
-//            return "thirdQuartileEvent"
-//        case .unmuteEvent:
-//            return "unmuteEvent"
-//        case .pauseEvent:
-//            return "pauseEvent"
-//        case .rewindEvent:
-//            return "rewindEvent"
-//        case .resumeEvent:
-//            return "resumeEvent"
-//        case .skipEvent:
-//            return "skipEvent"
-//        case .progressEvent:
-//            return "progressEvent"
-//        case .nonLinearClickTrackingEvent:
-//            return "nonLinearClickTrackingEvent"
-//        @unknown default:
-//            return "default"
-//        }
-//    }
-    // swiftlint:enable cyclomatic_complexity
-
     static func convertToDictionary(text: String) -> [String: Any]? {
         if let data = text.data(using: .utf8) {
             do {
