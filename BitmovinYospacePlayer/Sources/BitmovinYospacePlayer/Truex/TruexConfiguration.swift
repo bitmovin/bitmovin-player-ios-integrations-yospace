419680ee0112453afe77ed3dc5a6f7fc0a726a11
David Steinacher <david.steinacher@bitmovin.com>
2026-04-01 10:15:53 +0200

    properly support TrueX on tvOS

--- BitmovinYospacePlayer/Sources/BitmovinYospacePlayer/Truex/TruexConfiguration.swift
+++ BitmovinYospacePlayer/Sources/BitmovinYospacePlayer/Truex/TruexConfiguration.swift
@@ -2,7 +2,11 @@ import Foundation
 import UIKit
 
 public class TruexConfiguration {
+#if os(iOS)
     public private(set) var view: UIView
+#else
+    public private(set) var view: UIViewController
+#endif
     public private(set) var userId: String
     public private(set) var vastConfigUrl: String
 
@@ -16,9 +20,17 @@ public class TruexConfiguration {
      - userId:
      - vastConfigUrl:
      */
+#if os(iOS)
     public init(view: UIView, userId: String = "", vastConfigUrl: String = "") {
         self.view = view
         self.userId = userId
         self.vastConfigUrl = vastConfigUrl
     }
+#else
+    public init(view: UIViewController, userId: String = "", vastConfigUrl: String = "") {
+        self.view = view
+        self.userId = userId
+        self.vastConfigUrl = vastConfigUrl
+    }
+#endif
 }
